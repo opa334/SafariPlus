819bd19ca65e8dcd48982454f0eddd0632a97026
opa334 <opa334@users.noreply.github.com>
2021-03-29 22:18:46 +0200

    before I forget it again...

--- Preferences/SPPUploadDownloadListController.mm
+++ Preferences/SPPUploadDownloadListController.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020 Lars Fröder
+// Copyright (c) 2017-2021 Lars Fröder
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
